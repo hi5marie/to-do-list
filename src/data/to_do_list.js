63aed0748c243ad5ee78dd10162bedd69baebab0
mterrado <mcterrado@me.com>
2018-12-19 16:32:39 -0800

    Completed displaying list items with dummy data

---
+++ src/data/to_do_list.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        _id: '001',
+        title: 'Wash Car',
+        details: 'Wash the blue car',
+        complete: false
+    },
+
+    {
+        _id: '002',
+        title: 'Do Laundry',
+        details: 'Wash clothes in laundry',
+        complete: false
+    },
+
+    {
+        _id: '003',
+        title: 'Work out',
+        details: 'Go to gym',
+        complete: false
+    },
+
+    {
+        _id: '004',
+        title: 'Finish Homework',
+        details: 'Work on prototypes',
+        complete: false
+    }
+
+];
