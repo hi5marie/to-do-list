da852c556fca7a315d6bf4e0c2c1e0504000c6c5
mterrado <mcterrado@me.com>
2018-12-21 13:14:57 -0800

    branch connect-to-server

--- src/components/app.js
+++ src/components/app.js
@@ -1,34 +1,58 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize';
 import React, { Component } from 'react';
+import axios from 'axios';
 import List from './list';
 import AddItem from './add_item';
 import dummyList from '../data/to_do_list';
 import { randomString } from '../helpers';
 
+const BASE_URL = 'http://api.reactprototypes.com/todos';
+const API_KEY = '?key=hi5marie';
+
 class App extends Component {
     state = {
         list: []
     }
-
     componentDidMount() {
         this.getListData();
     }
+    addItem = async (item) => {
+        const resp = await axios.post(BASE_URL + API_KEY, item);
+        this.getListData();
 
-    addItem = (item) => {
-        const { list } = this.state;
-
-        this.setState({
-            list: [{...item, _id: randomString()}, ...list]
-        });
     }
 
-    getListData() {
-        // Call server to get list data
+    async getListData() {
 
-        this.setState({
-            list: dummyList
-        });
+        try{
+
+            const resp = await axios.get(BASE_URL + API_KEY);
+
+            this.setState({
+                list: resp.data.todos
+            });
+
+        }catch(err){
+            console.log('Something went wrong in getListData')
+        }
+
+    }
+
+    deleteItem = async (id) => {
+
+        await axios.delete(`${BASE_URL}/${id + API_KEY}`);
+
+        this.getListData();
+
+    }
+
+    toggleComplete = async (id) => {
+
+        await axios.put(`${BASE_URL}/${id + API_KEY}`);
+
+        this.getListData();
+
     }
 
     render(){
@@ -39,10 +63,13 @@ class App extends Component {
                 <h1 className="center">To Do List</h1>
                 
                 <AddItem add={this.addItem}/>
-                <List toDos={list}/>
+                <List delete={this.deleteItem} toDos={list}/>
+
             </div>
         );
     }
 }
 
 export default App;
+
+
